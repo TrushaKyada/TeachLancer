fa0035085d71915a1f2fdc0be49ee9b217639676
TrushaKyada <108670762+TrushaKyada@users.noreply.github.com>
2023-05-15 22:34:42 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", true)
 
-mongoose.connect("mongodb+srv://teachLancer:teachLancer@cluster0.ktqzdyg.mongodb.net/teach-lancer").
-then(()=>{
+mongoose.connect(process.env.db).then(()=>{
     console.log("Database Connected!")
     console.log("=========================================================================");
 }).catch((err)=>{
     console.log("Database Not Connected!")
 })
 
-// mongodb+srv://teachLancer:teachLancer@cluster0.ktqzdyg.mongodb.net/teach-lancer   
+  
